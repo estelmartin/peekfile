32da2ecf58ee70e350c74eb6bf607e4154c1e347
Estel Martin <estel.m.2001@gmail.com>
2023-11-01 21:28:28 +0100

    fourth version (2/11)

--- peek.sh
+++ peek.sh
@@ -1,6 +1,10 @@
 if [[ -z $2 ]] ; then numlines=3 ; else numlines=$2 ; fi 
 
-head -n $numlines $1 
-echo "..."
-tail -n $numlines $1 
-
+WC=$( cat $1 | wc -l)
+if [[ $WC -le  $(( $numlines * 2 )) ]] ; then cat $1  
+else 
+	echo warning: too many lines
+	head -n $numlines $1 
+	echo "..."
+	tail -n $numlines $1 
+fi
