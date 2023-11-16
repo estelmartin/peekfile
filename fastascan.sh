290547f8dda5f5461ec81036cf35ddb7e4f939bd
Estel Martin <estel.m.2001@gmail.com>
2023-11-16 13:53:27 +0100

    version 2 (16/11)

--- fastascan.sh
+++ fastascan.sh
@@ -1,7 +1,15 @@
 #how many fasta/fa files are in X folder? 
 if [[ -z $1 ]]; then X=$1 ; else X=.; fi
 
-echo The total number of fasta/fa files in X folder is: $(find $X -type f -name "*.fasta" -or -type f -name "*.fa" | wc -l)
+echo The total number of fasta/fa files in X folder is:
+fastafiles=$(find $X -type f -name "*.fasta" -or -type f -name "*.fa") 
+echo $fastafiles | wc -w
 
+touch fastaIDs 
+for file in $fastafiles;
+do 
+awk '$1~/>/{print $1}' $file >> fastaIDs
+done
 
-
+echo The number of unique fasta IDs is:
+sort fastaIDs | uniq | wc -l
