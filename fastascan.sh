e9cd0fbb88a30efa06d98413110f68ecc6d1a7a2
Estel Martin <estel.m.2001@gmail.com>
2023-11-16 13:31:40 +0100

    version 1 (16/11)

--- fastascan.sh
+++ fastascan.sh
@@ -1,1 +1,7 @@
+#how many fasta/fa files are in X folder? 
+if [[ -z $1 ]]; then X=$1 ; else X=.; fi
+
+echo The total number of fasta/fa files in X folder is: $(find $X -type f -name "*.fasta" -or -type f -name "*.fa" | wc -l)
+
+
 
