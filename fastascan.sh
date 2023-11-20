c1edad320623f0337021a19ed86129ddcc4df5f8
Estel Martin <estel.m.2001@gmail.com>
2023-11-20 10:01:43 +0100

    version 4

--- fastascan.sh
+++ fastascan.sh
@@ -4,7 +4,7 @@ if [[ -z $1 ]]; then X=$1 ; else X=.; fi
 echo The total number of fasta/fa files \(and symlinks\) in the folder is:
 fastafiles=$(find $X -type f,l -name "*.fasta" -or -type f,l -name "*.fa") 
 echo $fastafiles | wc -w
-#teh same bash has suggested me to use a comma "," to separate conditions of "type"
+#the same bash has suggested me to use a comma "," to separate conditions of "type"
 
 touch fastaIDs 
 for file in $fastafiles;
@@ -24,4 +24,6 @@ if [[ -h $file ]]; then
 	echo The file is a symlink; else
 	echo The file is NOT a symlink 
 fi
+echo The number of sequences it contains is: 
+awk '$1~/>/{print NR}' $file | wc -l
 done
