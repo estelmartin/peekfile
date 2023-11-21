5ee993326642de7bfdabb0517c4611bed3a6fc1f
Estel Martin <estel.m.2001@gmail.com>
2023-11-21 18:18:00 +0100

    version 8

--- fastascan.sh
+++ fastascan.sh
@@ -1,7 +1,8 @@
 #how many fasta/fa files are in X folder? 
-if [[ -z $1 ]]; then X=$1 ; else X=.; fi
+if [[ -z $1 ]]; then X=. ; else X=$1; fi
 
 fastafiles=$(find $X -type f,l -name "*.fasta" -or -type f,l -name "*.fa") 
+echo DIRECTORY OVERVIEW:
 echo The total number of fasta/fa files \(and symlinks\) in the folder is: $(echo $fastafiles | wc -w)
 #the same bash has suggested me to use a comma "," to separate conditions of "type"
 
@@ -16,15 +17,17 @@ echo "" # empty line to separate
 
 rm fastaIDs
 
-#for each file
-echo Characteristics of each file: 
+
+echo FILES INFORMATION: 
+
+#FOR EACH FILE
 for file in $fastafiles;
 do 
 
+if [[ ! -r $file ]]; then echo --\>$file THIS FILE IS NOT READABLE, it will be skipped. To make it readable, change permissions \(chmod a+r $file\); echo ""; else 
 #define a variable called header with the name of the file
-
-#determine if the file is a symlink or not and add this to the variable
 header=--">"$file 
+#determine if the file is a symlink or not and add this to the variable
 if [[ -h $file ]]; then 
 	header=$header$'\t'"| "Symlink; else
 	header=$header$'\t'"| "no_simlink
@@ -32,17 +35,31 @@ fi
 
 #determine the number of sequences
 
-if [[ $(grep ">" $file | wc -l) -eq 0 ]]; then header=$header$'\t'"| ""It is not a current file, it is a binary file"; echo $header; echo ""; continue; 
+if [[ $(grep -c ">" $file) -eq 0 ]]; then header=$header$'\t'"| ""WARNING! It is not a current file, it is a binary file"; echo $header; echo ""; continue; 
 else header=$header$'\t'"| "Num_Sequences:$(awk '$1~/>/' $file | wc -l); fi
 
 #determine the total sequences length
 header=$header$'\t'"| "Total_Length:$(awk '!($1~/>/)' $file | tr -d '\n' | sed 's/-//g' | wc -c)
 
-#detemrine if the file is nucleotide or amino acid
-if [[ $(awk '!($1~/>/)' $file)==[A,G,T,C] ]]; then header=$header$'\t'"| "Nucleotide
+#determine if the file has nucleotides or amino acids
+if [[ $(awk '!($1~/>/)' $file)==[AGTCNU] ]]; then header=$header$'\t'"| "Nucleotide
 else header=$header$'\t'"| "Amino_acid
 fi
 
+#content display
+if [[ -z $2 ]]; then N=0 ; else N=$2; fi
+
 echo $header
 echo "" #empty line to facilitate visualization
+
+if [[ $(cat $file | wc -l) -le $(($N * 2)) ]]; then cat $file; echo ""
+elif [[ $(cat $file | wc -l) -gt $(($N * 2)) ]]; then 
+head -n $N $file
+echo ...
+tail -n $N $file
+echo ""
+elif [[ $N -eq 0 ]]; then continue
+fi
+
+fi
 done
