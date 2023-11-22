7d1ed5a16bc10cf26108d3728964ab28ac19c363
Estel Martin <estel.m.2001@gmail.com>
2023-11-22 10:24:20 +0100

    version 11

--- fastascan.sh
+++ fastascan.sh
@@ -10,8 +10,7 @@ echo The total number of fasta/fa files \(and symlinks\) in the folder is: $(ech
 
 ##count the total number of fasta IDs in the folder
 touch fastaIDs 
-for file in $fastafiles;
-do 
+for file in $fastafiles; do 
 awk '$1~/>/{print $1}' $file >> fastaIDs
 done
 
@@ -24,48 +23,48 @@ rm fastaIDs
 echo FILES INFORMATION: 
 
 ##loop through each file of the fastafind variable (which contains all the fa/fasta files in that directory) to make a nice header with some file information, followed by (some of) the content of each file
-for file in $fastafiles;
+for file in $fastafiles; 
 do 
 
 #define a variable called header with the name of the file
 header=---\>$file 
 
-#skip non readable files, for the readable ones do the for loop
-if [[ ! -r $file ]]; then echo $header THIS FILE IS NOT READABLE, it will be skipped. To make it readable, change permissions \(chmod a+r $file\); echo ""; else 
+	#skip non readable files, for the readable ones do the for loop
+if [[ ! -r $file ]]; then echo $header THIS FILE IS NOT READABLE, it will be 	skipped. To make it readable, change permissions \(chmod a+r $file\); echo ""; else 
 
-#determine if the file is a symlink or not and add this information to the variable "header"
-if [[ -h $file ]]; then 
-	header=$header$'\t'"| "Symlink; else
-	header=$header$'\t'"| "No_simlink
-fi 
+	#determine if the file is a symlink or not and add this information to the 	variable "header"
+	if [[ -h $file ]]; then 
+		header=$header$'\t'"| "Symlink; else
+		header=$header$'\t'"| "No_simlink
+	fi 
 
-#determine the number of sequences of the file. If we find a binary file, 
-if [[ $(grep -c ">" $file) -eq 0 ]]; then header=$header$'\t'"| ""WARNING! It is not an ordinary file, it is a binary file"; echo $header; echo ""; continue; 
-else header=$header$'\t'"| "Num_Sequences:$(awk '$1~/>/' $file | wc -l); fi
+	#determine the number of sequences of the file. If we find a binary file, 
+	if [[ $(grep -c ">" $file) -eq 0 ]]; then header=$header$'\t'"| ""WARNING! It is not an ordinary file, it is a binary file"; echo $header; echo ""; continue; 
+	else header=$header$'\t'"| "Num_Sequences:$(awk '$1~/>/' $file | wc -l); fi
 
-#determine the total sequences length
-header=$header$'\t'"| "Total_Length:$(awk '!($1~/>/)' $file | tr -d '\n' | sed 's/-//g' | wc -c)
+	#determine the total sequences length
+	header=$header$'\t'"| "Total_Length:$(awk '!($1~/>/)' $file | tr -d '\n' | sed 's/-//g' | wc -c)
 
-#determine if the file has nucleotides or amino acids
-if [[ $(awk '!($1~/>/)' $file | grep -ic [QWERTYIPASDFGHKLCVNM] ) -gt $(awk '!($1~/>/)' $file | grep -ic  [ACTGNU]) ]]; then header=$header$'\t'"| "Amino_Acid
-else header=$header$'\t'"| "Nucleotide
-fi 
+	#determine if the file has nucleotides or amino acids
+	if [[ $(awk '!($1~/>/)' $file | grep -io [QWERTYIPASDFGHKLCVNM] | wc -l ) -gt $(awk '!($1~/>/)' $file | grep -io  [ACTGNU] | wc -l ) ]]; then header=$header$'\t'"| "Amino_Acid
+	else header=$header$'\t'"| "Nucleotide
+	fi 
 
-#print the "header"
-echo $header
+	#print the "header"
+	echo $header
 
-##content display
-#define the number of lines N
-if [[ -z $2 ]]; then N=0 ; else N=$2; fi
+	##content display
+	#define the number of lines N
+	if [[ -n $2 ]]; then N=$2 ; else N=0; fi
 
-if [[ $N -eq 0 ]]; then echo ""; continue
-else
-if [[ $(cat $file | wc -l) -gt $(($N * 2)) ]]; then 
-head -n $N $file && echo ... && tail -n $N $file; echo ""
-else
-cat $file; echo ""
-fi
-fi
+	#display all or part of the content depend on the file length
+	if [[ $N -eq 0 ]]; then echo ""; continue
+	elif [[ $(cat $file | wc -l) -gt $(($N * 2)) ]]; then 
+	head -n $N $file && echo ... && tail -n $N $file; echo ""
+	else
+	cat $file; echo ""
+	fi
+	
 
 fi
 done
